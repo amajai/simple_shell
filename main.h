8ec1dd165f2ee5d450ec97a3f850defa22a0a371
Abdul <aabdulmajeed.isa@gmail.com>
2023-07-14 17:14:39 +0100

    feature/handle command lines with arguments

--- main.h
+++ main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 char *_strdup(const char *str);
 unsigned int _strlen(const char *str);
+void execute(char *ppath, char *cmd_input, char **env, char *execname);
 #endif
--- shell.c
+++ shell.c
@@ -12,23 +12,19 @@
  * @env: bash enviroments
  * Return: 0
  */
-
 int main(int ac, char **av, char **env)
 {
 	size_t n = 0;
 	pid_t pid;
-	char *buffer = NULL, **cmds = NULL, *token, *dupbuff;
+	char *buffer = NULL, *token, *dupbuff;
 	int cmd, status;
 
 	(void)ac;
 	isatty(STDIN_FILENO) ? write(1, "$ ", 2) : write(1, "", 0);
 	while ((cmd = getline(&buffer, &n, stdin)) != EOF)
 	{
-		cmds = malloc(sizeof(char *) * 2);
 		dupbuff = _strdup(buffer);
-		token = strtok(dupbuff, "\n");
-		cmds[0] = token;
-		cmds[1] = NULL;
+		token = strtok(dupbuff, " \n");
 		pid = fork();
 		if (pid == -1)
 		{
@@ -36,22 +32,55 @@ int main(int ac, char **av, char **env)
 			return (1);
 		}
 		else if (pid == 0)
-		{
-			if (execve(cmds[0], cmds, env) == EOF)
-			{
-				perror(av[0]);
-				free(dupbuff);
-				free(cmds);
-				free(buffer);
-				return (1);
-			}
-		}
+			execute(token, buffer, env, av[0]);
 		else
 			wait(&status);
-		free(cmds);
 		free(dupbuff);
 		isatty(STDIN_FILENO) ? write(1, "$ ", 2) : write(1, "", 0);
 	}
 	free(buffer);
 	return (0);
+}
+
+/**
+ * execute - execute program through execve function for a given path.
+ * @ppath: program path name.
+ * @buffer: stored input line from stdin stream.
+ * @env: shell environment list.
+ * @execname: Name of the executable the shell is run from.
+ *
+ * Return: Nothing
+ */
+void execute(char *ppath, char *buffer, char **env, char *execname)
+{
+	int i = 0, j = 0, arg_count = 0;
+	char **argv;
+	char *token;
+
+	while (buffer[j] != '\0')
+	{
+		if (buffer[j] == ' ')
+			arg_count++;
+		j++;
+	}
+	arg_count++;
+	argv = malloc(sizeof(char *) * (arg_count + 1));
+	if (argv == NULL)
+		exit(EXIT_FAILURE);
+	argv[i] = ppath;
+	token = strtok(buffer, " \n");
+	while (token != NULL)
+	{
+		token = strtok(NULL, " \n");
+		argv[++i] = token;
+	}
+	argv[i] = NULL;
+	if (execve(argv[0], argv, env) == EOF)
+	{
+		perror(execname);
+		free(ppath);
+		free(argv);
+		free(buffer);
+		exit(EXIT_FAILURE);
+	}
 }
