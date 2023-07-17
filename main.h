31a7f2f2e92c6dea4b2f993c05aeee4c3a13aa96
MK-Aaron <mwendwa_aaron78@yahoo.com>
2023-07-17 14:06:57 +0300

    Update: Header File

--- main.h
+++ main.h
@@ -5,8 +5,9 @@ unsigned int _strlen(const char *str);
 void execute(char *ppath, char *cmd_input, char **env, char *execname);
 extern char **environ;
 char *_getenv(char *name);
-char *process_input(char *pname);
+char *process_input(char *pname, char *buffer);
 char *_strcpy(char *copy, char *origin);
 char *joinpath(char *dest, char *s1, size_t s1_len, char *s2, size_t s2_len);
 int _strncmp(char *s1, char *s2, int len);
+int _strcmp(char *s1, char *s2);
 #endif
