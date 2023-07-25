83085ded3fe427481790324d3c4a5233ff483e48
Abdul <aabdulmajeed.isa@gmail.com>
2023-07-26 00:16:32 +0100

    fix function

--- fflush.c
+++ fflush.c
@@ -11,22 +11,23 @@
 
 void error_disp(char *cmd, unsigned int count, char *arg)
 {
-    char count_str[4]; 
-    int temp = count - 1;
-    int digit_count = 0;
+	char count_str[4];
+	int temp = count - 1;
+	int digit_count = 0;
 
-    while (temp != 0)
+	while (temp != 0)
 	{
-        digit_count++;
-        temp /= 10;
+		digit_count++;
+		temp /= 10;
 	}
-    temp = count - 1;
-    count_str[digit_count] = '\0'; 
-    while (digit_count > 0) {
-        digit_count--;
-        count_str[digit_count] = temp % 10 + '0';
-        temp /= 10;
-    }
+	temp = count - 1;
+	count_str[digit_count] = '\0';
+	while (digit_count > 0)
+	{
+		digit_count--;
+		count_str[digit_count] = temp % 10 + '0';
+		temp /= 10;
+	}
 	write(2, arg, _strlen(arg));
 	write(2, ": ", 2);
 	write(2, count_str, _strlen(count_str));
